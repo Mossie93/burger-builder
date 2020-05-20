f944161b3f075b8edd077bdb4816e54665929d71
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-20 23:21:33 +0200

    Test Layout component in real life

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Componnent {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
