7fc2f2f508332df4f034bc77449860858d396735
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-21 20:32:20 +0200

    Fix bread-top className

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -11,7 +11,7 @@ const burgerIngredient = (props) => {
       break;
     case('bread-top'):
       ingredient = (
-        <div className={classes.BreadBottom}>
+        <div className={classes.BreadTop}>
           <div className={classes.Seeds1}></div>
           <div className={classes.Seeds2}></div>
         </div>
